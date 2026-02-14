b0c82daf69ec89c8341425d0055ab183a189b296
Jorge Daniel Calderon <122463446+jd-kalde87@users.noreply.github.com>
2026-02-14 13:41:15 -0500

    Remove port configuration from database pool

--- actas_backend/config/db.js
+++ actas_backend/config/db.js
@@ -13,7 +13,6 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
 });
 
 // 4. Exportamos una versión del pool que nos permite usar la sintaxis moderna async/await
